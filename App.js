05940d1417ccc0ad78e4ca8c65dcd36a433f64df
Chris Monteiro <chris@viperapps.net>
2018-01-31 16:35:51 -0500

    Initial FaceBook Auth

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as firebase from 'firebase';
+import loginFacebook from './components/login';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,11 +12,40 @@ const styles = StyleSheet.create({
   },
 });
 
+function storeHighScore(user, score) {
+  if (user != null) {
+    firebase
+      .database()
+      .ref(`user/${user.uid}`)
+      .set({
+        highscore: score,
+      });
+  }
+}
+
 export default class App extends React.Component {
+  componentDidMount() {
+    // Initialize Firebase
+    const firebaseConfig = {
+      apiKey: 'AIzaSyB2ZZEFkIIFeW_un8kSkaEocEgaDGMuxIU',
+      authDomain: 'pinpointit-393d2.firebaseapp.com',
+      databaseURL: 'https://pinpointit-393d2.firebaseio.com/',
+      storageBucket: 'pinpointit-393d2.appspot.com',
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome to PinpointIt</Text>
+        <Button
+          onPress={loginFacebook}
+          title="Login with FaceBook"
+          color="#4286f4"
+          accessibilityLabel="Learn more about this blue button"
+        />
       </View>
     );
   }
